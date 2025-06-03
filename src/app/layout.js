a6fa1831250178ea8320e980fa3f74b2b3c21c98
Nithin9585 <nithin958595@gmail.com>
2025-06-03 15:26:57 +0530

    altration to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,14 +21,33 @@ export const metadata = {
   },
   openGraph: {
     title: "Ai Quiz App",
-    description: "Automatically generate quizzes from PDFs & PPTX using Gemini AI and Google Forms| Ai Quiz App by Nithin N VIT Bhopal",
+    description: "Automatically generate quizzes from PDFs & PPTX using Gemini AI and Google Forms | Ai Quiz App by Nithin N VIT Bhopal",
     url: "https://ai-quiz-app-nu.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://ai-quiz-app-nu.vercel.app/Nithin.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Ai Quiz App preview image",
+      },
+    ],
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ai Quiz App",
+    description: "Automatically generate quizzes from PDFs & PPTX using Gemini AI and Google Forms | Ai Quiz App by Nithin N VIT Bhopal",
+    images: ["https://ai-quiz-app-nu.vercel.app/Nithin.jpg"],
+    creator: "@NithinN356853",
+  },
+  viewport: "width=device-width, initial-scale=1",
+  keywords: ["AI", "Quiz Generator", "Gemini", "PDF to Quiz", "Google Forms"],
+  authors: [{ name: "Nithin N", url: "https://www.linkedin.com/in/nithin-n-a4b837275/" }],
+  robots: "index, follow",
   other: {
     "google-site-verification": "9__SOhCWiYz4dCQT6b3a2jexCJPzu7oEFD8EhnB2J9A",
   },
 };
-
 
 
 export default async function RootLayout({ children }) {
