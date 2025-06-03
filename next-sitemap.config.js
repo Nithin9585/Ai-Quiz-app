c27a4fad3da3af2e331f66aa40acc45986136dcb
Nithin9585 <nithin958595@gmail.com>
2025-06-03 14:55:21 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://ai-quiz-app-nu.vercel.app', // ✅ Your deployed URL
+  generateRobotsTxt: true, 
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
